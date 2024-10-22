5f7741b555ff0ce2c126551f78711fde8f440967
karahmon <monilkarania@gmail.com>
2024-10-22 16:25:26 +0530

    Updated Test Library

--- src/test/java/com/example/bookauthorapp/repository/AuthorRepositoryTest.java
+++ src/test/java/com/example/bookauthorapp/repository/AuthorRepositoryTest.java
@@ -4,6 +4,7 @@ import com.example.bookauthorapp.model.Author;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class AuthorRepositoryTest {
 
     @Autowired
--- src/test/java/com/example/bookauthorapp/repository/BookRepositoryTest.java
+++ src/test/java/com/example/bookauthorapp/repository/BookRepositoryTest.java
@@ -5,6 +5,7 @@ import com.example.bookauthorapp.model.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @DataJpaTest
+@ActiveProfiles("test")
 public class BookRepositoryTest {
 
     @Autowired
